147f08e18465664a093973fcbe5dbdcddfbb9054
Mishan Poudel <poudelmishan2@gmail.com>
2024-02-19 21:40:23 +0000

    feat: Implement Express router and API routes

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutsRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
 
 // middlware
+app.use(express.json())
+
 app.use((req,res,next)=>{
     console.log(req.path, req.method)
     next()
 })
 
 // routes
-app.get(`/`,(req, res)=>{
-    res.json({msg:`Welcome to the app`})
-})
+app.use('/ninja/workouts',workoutsRoutes)
 
 // listen for requests
 app.listen(process.env.PORT,()=>{
