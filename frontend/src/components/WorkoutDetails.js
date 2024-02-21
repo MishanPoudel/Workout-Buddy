abd174ac1ef114a0577b46644cc00c21c0b49ea7
Mishan Poudel <poudelmishan2@gmail.com>
2024-02-21 11:52:00 +0000

    feat: Implement error response handling

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -4,7 +4,7 @@ const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutsContext()
 
   const handleClick = async () => {
-    const response = await fetch('/api/workouts/' + workout._id, {
+    const response = await fetch('http://localhost:4000/api/workouts/' + workout._id, {
       method: 'DELETE'
     })
     const json = await response.json()
